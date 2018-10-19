dca9620894f7a6c507f49aa97139e1165b2263cf
Graeme Congreve <junkmailgmc@gmail.com>
2018-10-19 13:57:13 +0100

    answered first 7 questions

--- start_point/pet_shop.rb
+++ start_point/pet_shop.rb
@@ -9,3 +9,11 @@ end
 def add_or_remove_cash(pet_shop_hash, cash_amount)
   return pet_shop_hash[:admin][:total_cash] += cash_amount
 end
+
+def pets_sold(pet_shop_hash)
+  return pet_shop_hash[:admin][:pets_sold]
+end
+
+def increase_pets_sold(pet_shop_hash, pets_sold)
+  return pet_shop_hash[:admin][:pets_sold] += pets_sold
+end
